a2f8d2bfd7f7bd97bc767b9b131050235c00d005
gwwwwt <gwwwwt@hotmail.com>
2024-01-19 02:14:02 +0800

    代码修改 + log4j 配置

--- src/main/java/org/bc/fenfencai/DataReceiver.java
+++ src/main/java/org/bc/fenfencai/DataReceiver.java
@@ -1,6 +1,7 @@
 package org.bc.fenfencai;
 
 import com.binance.connector.client.impl.spot.Market;
+import org.jetbrains.annotations.NotNull;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -8,36 +9,44 @@ import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
 public class DataReceiver {
+    private final static long MS_OF_MINUTE = 60 * 1000;
+
     public static void main(String[] args) throws InterruptedException {
         Market market = MarketFactory.newInstance();
 
+        long start = (System.currentTimeMillis() / MS_OF_MINUTE * MS_OF_MINUTE) + MS_OF_MINUTE;
+
+        while (true) {
+            TimeUnit.MILLISECONDS.sleep(start + 2000 - System.currentTimeMillis());
+
+            LocalDateTime today = LocalDateTime.now();
+
+            String currentLoop = today.format(DateTimeFormatter.ofPattern("yyyyMMdd"))
+                + String.valueOf(today.getHour() * 60 + today.getMinute());
+
+            System.out.print("当前期数:" + currentLoop + "------------");
+
+            HashMap<String, Object> parameters = getParameters(start);
+
+            // System.out.println(parameters);
+
+            String response = market.klines(parameters);
+
+            System.out.println(response.split(",")[7]);
+
+            start += (60 * 1000);
+        }
+    }
+
+    @NotNull
+    private static HashMap<String, Object> getParameters(long start) {
         HashMap<String, Object> parameters = new HashMap<>();
         parameters.put("symbol", BtcUsdtParamers.SYMBOL);
         parameters.put("interval", BtcUsdtParamers.INTERVAL);
         parameters.put("limit", BtcUsdtParamers.LIMIT);
 
-        String response;
-        long startOfCurrentMinute;
-        long startOfLastMinute;
-
-        TimeUnit.MILLISECONDS.sleep((System.currentTimeMillis() / (60 * 1000) * (60 * 1000) + (60 * 1000)) - System.currentTimeMillis());
-
-        while (true) {
-            String currentLoop = LocalDateTime.now().minusMinutes(1).format(DateTimeFormatter.ofPattern("yyyyMMddmm"));
-
-            System.out.println("当前期数:" + currentLoop + "------------");
-
-            startOfCurrentMinute = System.currentTimeMillis() / (60 * 1000) * (60 * 1000);
-            startOfLastMinute = startOfCurrentMinute - (60 * 1000);
-
-            parameters.put("startTime", startOfLastMinute);
-            parameters.put("endTime", startOfCurrentMinute);
-
-            response = market.klines(parameters);
-            System.out.println(response.split(",")[7]);
-
-            TimeUnit.MILLISECONDS.sleep((System.currentTimeMillis() / (60 * 1000) * (60 * 1000) + (60 * 1000)) - System.currentTimeMillis());
-        }
-
+        parameters.put("startTime", start - MS_OF_MINUTE);
+        parameters.put("endTime", start);
+        return parameters;
     }
 }
