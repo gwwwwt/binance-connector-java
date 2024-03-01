3aef7965e1d4ea04671a2c544c5933f3ae0f9924
gwwwwt <gwwwwt@hotmail.com>
2024-03-01 14:24:08 +0800

    修改版

--- src/main/java/org/bc/fenfencai/DataReceiver.java
+++ src/main/java/org/bc/fenfencai/DataReceiver.java
@@ -6,47 +6,81 @@ import org.jetbrains.annotations.NotNull;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class DataReceiver {
     private final static long MS_OF_MINUTE = 60 * 1000;
 
-    public static void main(String[] args) throws InterruptedException {
-        Market market = MarketFactory.newInstance();
+    private static final CyclicBarrier BARRIER = new CyclicBarrier(3, () -> {
+        System.out.println("两个线程都跑完了，主线程继续...");
+    });
 
-        long start = (System.currentTimeMillis() / MS_OF_MINUTE * MS_OF_MINUTE) + MS_OF_MINUTE;
+    public static void main(String[] args) throws InterruptedException, BrokenBarrierException {
+
+        ExecutorService threadPool = Executors.newFixedThreadPool(2);
+
+        Market market_for_thread1 = MarketFactory.newInstance();
+        Market market_for_thread2 = MarketFactory.newInstance();
+
+        long begin_of_next_minute = get_begin_of_next_minute();
 
         while (true) {
-            TimeUnit.MILLISECONDS.sleep(start + 2000 - System.currentTimeMillis());
+            threadPool.execute(getTask(market_for_thread1, begin_of_next_minute - MS_OF_MINUTE, begin_of_next_minute,
+                "准确值", true));
 
-            LocalDateTime today = LocalDateTime.now();
+            threadPool.execute(getTask(market_for_thread2, begin_of_next_minute - MS_OF_MINUTE,
+                begin_of_next_minute - 1500, "近似值", false ));
 
-            String currentLoop = today.format(DateTimeFormatter.ofPattern("yyyyMMdd"))
-                + String.valueOf(today.getHour() * 60 + today.getMinute());
-
-            System.out.print("当前期数:" + currentLoop + "------------");
-
-            HashMap<String, Object> parameters = getParameters(start);
-
-            // System.out.println(parameters);
-
-            String response = market.klines(parameters);
-
-            System.out.println(response.split(",")[7]);
-
-            start += (60 * 1000);
+            BARRIER.await();
+            BARRIER.reset();
+            begin_of_next_minute = get_begin_of_next_minute();
         }
     }
 
+    private static long get_begin_of_next_minute() {
+        return (System.currentTimeMillis() / MS_OF_MINUTE * MS_OF_MINUTE) + MS_OF_MINUTE;
+    }
+
+    private static Runnable getTask(Market market, long start_time, long end_time, String message, boolean isAccurate) {
+        return new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(end_time + 1000 - System.currentTimeMillis());
+
+                    LocalDateTime today = LocalDateTime.now();
+                    if (!isAccurate) {
+                        today = today.plusMinutes(1);
+                    }
+                    String currentLoop = today.format(DateTimeFormatter.ofPattern("yyyyMMdd"))
+                        + String.valueOf(today.getHour() * 60 + today.getMinute());
+
+                    HashMap<String, Object> parameters = getParameters(start_time, end_time, BtcUsdtParamers.INTERVAL);
+                    // System.out.println(parameters);
+
+                    String response = market.klines(parameters);
+                    System.out.printf("%35s", "第" + currentLoop + "期" + message + " --- ");
+
+                    String result = response.split(",")[7].trim();
+                    System.out.println(result.substring(1, result.length() - 1));
+
+                    BARRIER.await();
+                } catch (Exception e) {
+                    System.out.println("Interrupt exception ... Ignore it ......");
+                }
+            }
+        };
+    }
+
     @NotNull
-    private static HashMap<String, Object> getParameters(long start) {
+    private static HashMap<String, Object> getParameters(long start, long end, String interval) {
         HashMap<String, Object> parameters = new HashMap<>();
         parameters.put("symbol", BtcUsdtParamers.SYMBOL);
-        parameters.put("interval", BtcUsdtParamers.INTERVAL);
+        parameters.put("interval", interval);
         parameters.put("limit", BtcUsdtParamers.LIMIT);
 
-        parameters.put("startTime", start - MS_OF_MINUTE);
-        parameters.put("endTime", start);
+        parameters.put("startTime", start);
+        parameters.put("endTime", end);
         return parameters;
     }
 }
--- src/main/java/org/bc/fenfencai/MarketFactory.java
+++ src/main/java/org/bc/fenfencai/MarketFactory.java
@@ -9,20 +9,25 @@ import org.bc.Config;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
 
+/**
+ * 因为想要2个线程跑任务，所以取消使用单例
+ *
+ */
 public class MarketFactory {
-    private static Market INSTANCE;
+    //
+    // private static Market INSTANCE;
     public static Market newInstance() {
-        if (INSTANCE != null) {
-            return INSTANCE;
-        }
+        // if (INSTANCE != null) {
+        //     return INSTANCE;
+        // }
 
         SpotClient client = new SpotClientImpl(Config.API_KEY, Config.SECRET_KEY, Config.URL);
 //        SpotClient client = new SpotClientImpl(Config.API_KEY, Config.SECRET_KEY, Config.URL);
         Proxy proxyConn = new Proxy(Proxy.Type.SOCKS, new InetSocketAddress("127.0.0.1", 7890));
         ProxyAuth proxy = new ProxyAuth(proxyConn, null);
         client.setProxy(proxy);
-
-        INSTANCE = client.createMarket();
-        return INSTANCE;
+        // INSTANCE = client.createMarket();
+        // return INSTANCE;
+        return client.createMarket();
     }
 }
